f6228715b4fe11623aef1664f0897cd2ad83227e
Arvind <lordarvindthegamer@gmail.com>
2024-01-03 06:55:04 +0000

    Added: utils

---
+++ apps/model/utils.py
@@ -0,0 +1,14 @@
+from config.config import get_settings
+
+settings = get_settings()
+
+def write_file(file):
+    try:
+        with open(settings.AUDIO_DIR_PREDICTION + "/" + file.filename, "wb") as f:
+            f.write(file.file.read())
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
+
