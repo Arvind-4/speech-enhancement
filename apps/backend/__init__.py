e3dd64f87afbb8556b28a1f2d85a8daffafa5883
arvind-affinsys <arvind.a@affinsys.com>
2024-01-24 22:34:49 +0530

    Updated: main.py file for backend

--- apps/backend/__init__.py
+++ apps/backend/__init__.py
@@ -4,8 +4,8 @@ import pathlib
 BASE_DIR = pathlib.Path(__file__).parent.parent.parent
 
 paths = [
-    str(BASE_DIR / 'apps'),
-    str(BASE_DIR / 'apps' / 'backend'),
-    str(BASE_DIR / 'apps' / 'model'),
+    str(BASE_DIR / "apps"),
+    str(BASE_DIR / "apps" / "backend"),
+    str(BASE_DIR / "apps" / "model"),
 ]
 sys.path.extend(paths)
