4e00dba44002c1e86380f27baa5ffa96b3145a30
arvind-affinsys <arvind.a@affinsys.com>
2024-01-24 22:36:40 +0530

    Added: config file seperatly

--- apps/config/config.py
+++ apps/config/config.py
@@ -2,11 +2,14 @@ import pathlib
 from functools import lru_cache
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     BASE_DIR: pathlib.Path = pathlib.Path(__file__).resolve(strict=True).parent.parent
     MODEL_FILE: str = str(BASE_DIR) + "/" + "weights" + "/" + "model_unet.h5"
     AUDIO_DIR_PREDICTION: str = str(BASE_DIR) + "/" + "test_data" + "/" + "test"
-    DIR_SAVE_PREDICTION: str = str(BASE_DIR) + "/" + "test_data" + "/" + "saved_predictions"
+    DIR_SAVE_PREDICTION: str = (
+        str(BASE_DIR) + "/" + "test_data" + "/" + "saved_predictions"
+    )
     SAMPLE_RATE: int = 8000
     MIN_DURATION: float = 1.0
     FRAME_LENGTH: int = 8064
