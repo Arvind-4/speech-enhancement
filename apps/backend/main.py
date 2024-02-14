8caec62b029260b0fb8443958b8a40964aaf5b0f
Arvind-4 <pingarvindforquestions@gmail.com>
2024-02-15 00:01:03 +0530

    Updated: for display data

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,9 +1,11 @@
+import pathlib
 import tensorflow as tf
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 from model.utils import write_file
 from config.config import get_settings
+from backend.utils import generate_data
 from model.test_model import prediction_for_a_file
 
 
@@ -44,11 +46,15 @@ async def remove_noise(file: UploadFile = File(...)):
             file=file.filename,
             model=model,
         )
-        return {
-            "success": True,
-            "data": f"{output_file}",
-            "message": "Prediction completed",
-        }
+        print("Output file", output_file)
+        if output_file is None:
+            return {"success": False, "message": "Prediction failed", "data": None}
+        output_file_path = pathlib.Path(settings.DIR_SAVE_PREDICTION) / output_file
+        input_file_path = pathlib.Path(settings.AUDIO_DIR_PREDICTION) / file.filename
+        data = generate_data(input_file_path, output_file_path)
+        print("Data generated", data)
+        return {"success": True, "message": "Prediction successful", "data": data}
+
     except Exception as e:
         print(e)
         return {"success": False, "message": "Prediction failed", "data": None}
